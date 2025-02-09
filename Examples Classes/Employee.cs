419fcf75e7fb4db21a5fdc3596fce7512e125b15
20100 <mmmelkady23@gmail.com>
2025-02-09 13:30:56 +0200

    Add project files.

---
+++ Examples Classes/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Youtube_Videos_Advanced_c_.Examples_Classes
+{
+    internal class Employee:IComparable<Employee>
+    {
+        public  int ID { get; set; }
+        public string FullName { get; set; }
+        public Contract Contract { get; set; }
+        public decimal StartingSalary { get; set; }
+        public string DescriptionJob { get; set; }
+
+        //Constractor 
+        public Employee(int id,string name,Contract _contract,decimal Salary,string desc )
+        {
+            ID = id;
+            FullName = name;
+            Contract = _contract;
+            StartingSalary = Salary;
+            DescriptionJob = desc;
+        }
+
+        public override string ToString()
+        {
+            return $"ID : {ID}, FullName : {FullName} , Contract: {Contract},StartSalary {StartingSalary} ,Jop :{DescriptionJob}";
+        }
+
+        public int CompareTo(Employee? other)
+        {
+            return this.StartingSalary.CompareTo(other?.StartingSalary);
+        }
+    }
+
+    enum Contract
+    {
+        FullTime,
+        PartTime,
+        Causal,
+        Freelance
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Concurrent;
+using Youtube_Videos_Advanced_c_.Examples_Classes;
+
+namespace Youtube_Videos_Advanced_c_
+{
+    internal class Program
+    {
+        #region Content
+        //1 List
+        //2 List Of Objects
+        //3 Sort List OF Objects
+        #endregion
+        static void Main(string[] args)
+        {
+            #region Make List Of Employee
+            List<Employee> employees = new List<Employee>();
+            //Add Emplyee
+             employees.Add(new Employee(1, "mohamed Hesham", Contract.FullTime, 10000, "Developer"));
+             employees.Add(new Employee(2, "Amr ALi", Contract.PartTime, 6000, "Front End"));
+             employees.Add(new Employee(3, "omar Ahmed", Contract.Freelance, 5000, "Mobile Developer"));
+
+
+            Console.WriteLine("----------Befor Sorting List----------");
+            foreach (Employee employee in employees) 
+                Console.WriteLine(employee);
+
+            employees.Sort();//Sorting List Of Employees
+
+            Console.WriteLine("----------After Sorting List----------");
+            foreach (Employee employee in employees)
+                Console.WriteLine(employee);
+            #endregion
+        }
+    }
+}
